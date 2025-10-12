f2ef4897248709bf3e4b0dcff2c920f68d697765
Taslima30 <taslimabaig21@gmail.com>
2025-10-12 16:58:30 +0530

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -5,4 +5,4 @@ echo "starting new app.py..."
 cd /home/ec2-user/my-python-app/
 pip install -r requirements.txt -t
 sleep 5s
-nohup python3 app.py > logs.txt 2>&1 &
+nohup python3 taslima.py > logs.txt 2>&1 &
